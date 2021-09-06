254a54a5469b80f5c30a0a2932f695ee0ac0e969
nderimsali096 <63560554+nderimsali096@users.noreply.github.com>
2021-09-06 12:00:43 +0200

    adding routing,angular material and building the toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rest-countries-api-project';
+  mode = true;
+
+  changeMode() {
+    this.mode = !this.mode;
+  }
 }
